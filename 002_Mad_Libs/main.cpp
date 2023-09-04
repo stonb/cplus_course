b45a7d4737d3f1cfc0192135b1552dcbad519702
Stonb <stonb@hotmail.com>
2023-09-04 02:08:15 -0400

    Added project 002 - Mad libs

---
+++ 002_Mad_Libs/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+//This program simulates a simple Mad Libs game
+//User gets queried random words
+//Then a random story is written
+int main(){
+    std::string 
+        name = "",
+        color = "",
+        object = "",
+        animal = "",
+        profession = "",
+        instrument = "",
+        food = "";
+
+    std::cout << "Please type a person's name: ";
+    std::cin >> name;
+    std::cout << "Please type a color: ";
+    std::cin >> color;
+    std::cout << "Please type a object: ";
+    std::cin >> object;
+    std::cout << "Please type an animal: ";
+    std::cin >> animal;
+    std::cout << "Please type a profession/job: ";
+    std::cin >> profession;
+    std::cout << "Please type a instrument: ";
+    std::cin >> instrument;
+    std::cout << "Please type a food: ";
+    std::cin >> food;
+
+    std::cout << "Out of the " << color << ", " << name << " is being chased by an angry " << animal;
+    std::cout << ". In the background you see there is a " << profession << " playing a " << instrument;
+    std::cout << ". Suddenly, you wake up to the loud sound of a " << object << " falling on the floor!";
+    std::cout << " And you notice " << name << " eating a delicious " << food <<std::endl;
+
+    return 0;
+}
