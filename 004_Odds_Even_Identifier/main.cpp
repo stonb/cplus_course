8f26fd264055e45988ca035dbe4926d53ffe5946
Stonb <stonb@hotmail.com>
2023-09-04 21:52:25 -0400

    004 - Added Even Odd number checker

---
+++ 004_Odds_Even_Identifier/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//Program that checks if 10 numbers are either odd or even
+//Numbers are calculated randomly using srand/rand functions
+int main(){
+
+    srand(time(nullptr));
+
+    for (int i = 0; i < 10; i++)
+    {
+        int randomNumber = rand() % 51; // Random number between 0 and 50
+
+        if ((randomNumber % 2) == 0)
+        {
+            std::cout << (i + 1) << " Number "<< randomNumber << " is even!" << std::endl; 
+        }
+        else
+        {
+            std::cout << (i + 1) << " Number "<< randomNumber << " is odd!" << std::endl; 
+        }
+    }
+
+    return 0;
+}
